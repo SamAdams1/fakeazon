c91819ca1793b0b42fc9cac5c7bfeb5a63ed712c
Sam Adams <119465691+SamAdams1@users.noreply.github.com>
2024-05-21 15:55:16 -0400

    working api call

---
+++ src/App.jsx
@@ -0,0 +1,37 @@
+import { useState } from 'react'
+import './reset.css'
+import './App.css'
+
+async function search(value){
+  let data = await axios('https://dummyjson.com/products?limit=10&/search?q='+value)
+  console.log(data.data.products)
+}
+
+function App() {
+  const [value, setValue] = useState("");
+  let lastSearch;
+  
+  function handleSubmit(e){
+    e.preventDefault();
+    if (lastSearch != value){
+      lastSearch = value;
+      search(value);
+    }
+  }
+  
+  function handleChange(e){
+    setValue(e.target.value);
+  }
+
+  return (
+    <div className="App">
+      <h1>Fakeazon</h1>
+      <form onSubmit={handleSubmit}>
+        <input value={value} onInput={handleChange}required />
+        <button>Search</button>
+      </form>
+    </div>
+  )
+}
+
+export default App
