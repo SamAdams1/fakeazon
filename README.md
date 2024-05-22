6bc49aa7fe1a65c261e4a36a9ea11b456ddf16b4
Sam Adams <119465691+SamAdams1@users.noreply.github.com>
2024-05-22 17:41:57 -0400

    created build

---
+++ README.md
@@ -0,0 +1,7 @@
+# Fakeazon
+
+https://dummyjson.com/
+https://fakestoreapi.com/
+
+https://rapidapi.com/apimaker/api/walmart2/
+https://rapidapi.com/apidojo/api/unofficial-shein/
--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,25 @@
 import { useState } from 'react'
 import './reset.css'
 import './App.css'
-// hi
+
+// const options = {
+//   method: 'GET',
+//   url: 'https://amazon-data-scraper124.p.rapidapi.com/search/Nintendo%20Switch',
+//   params: {
+//     api_key: '3ba0325127417000e0b265200ab7ed34'
+//   },
+//   headers: {
+//     'X-RapidAPI-Key': '314eca509fmsh14eb0792cb112dfp132576jsn6244027cc714',
+//     'X-RapidAPI-Host': 'amazon-data-scraper124.p.rapidapi.com'
+//   }
+// };
+const options ='https://dummyjson.com/products'
+
 async function search(value){
-  let data = await axios('https://dummyjson.com/products?limit=10&/search?q=' + value)
-  console.log(data.data.products)
+  console.log(value)
+  // options.params.search = value
+  let data = await axios(options)
+  console.log(value, data.data.products)
 }
 
 function App() {
